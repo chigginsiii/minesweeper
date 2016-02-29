8fbed82a4f6bdb098c3b3b997a615004372eec5e
chigginsiii <chiggins@chiggins.com>
2016-02-29 12:40:20 -0800

    board dimensions

--- lib/minesweeper/entity/board_entity.rb
+++ lib/minesweeper/entity/board_entity.rb
@@ -1,7 +1,13 @@
 module Minesweeper
 	class BoardEntity
+		attr_reader :rows, :cols
 
-		def initialize
+		DefaultRows = 6
+		DefaultCols = 4
+
+		def initialize (rows: DefaultRows, cols: DefaultCols)
+			@rows = rows
+			@cols = cols
 		end
 
 	end
--- spec/minesweeper/entity/board_entity_spec.rb
+++ spec/minesweeper/entity/board_entity_spec.rb
@@ -2,8 +2,30 @@ require 'spec_helper'
 
 RSpec.describe Minesweeper::BoardEntity do
 	describe '#new' do
-		it 'creates a new board' do
-			expect(described_class.new).to be_a_kind_of Minesweeper::BoardEntity
+		context 'without params' do
+			let(:board) { described_class.new }
+
+			it 'creates a new board' do
+				expect(described_class.new).to be_a_kind_of Minesweeper::BoardEntity
+			end
+
+			it 'has a default dimension' do
+				expect(board.rows).to eq Minesweeper::BoardEntity::DefaultRows
+			end
+		end
+
+		context 'with params' do
+			let(:rows) { 7 }
+			let(:cols) { 5 }
+			let(:board) { described_class.new(rows: rows, cols: cols) }
+
+			it 'sets up rows' do
+				expect(board.rows).to eq rows
+			end
+
+			it 'sets up columns' do
+				expect(board.cols).to eq cols
+			end		
 		end
 	end
 end
