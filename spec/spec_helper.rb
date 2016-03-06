72183331182b95ada8e2b5917bc3794f0125ad34
chigginsiii <chiggins@chiggins.com>
2016-03-05 17:09:30 -0800

    cell_entity specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
+require 'simplecov'
+SimpleCov.start
+
 require 'rspec'
 require 'pp'
 require 'minesweeper'
 require 'benchsweeper'
-
-require 'simplecov'
-SimpleCov.start
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
